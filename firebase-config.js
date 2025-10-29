dbee7bb0c03629903bc248844846fb360f1ac06f
Muhammad Salman Azizi <muhammad.122140131@student.itera.ac.id>
2025-10-29 12:11:52 +0700

    Fix: Add firebase-config.js and mobile Safari safe-area support

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBM-4yu0dHNiK2pkai2AY0mncNMWgO_fhs",
+  authDomain: "jomblo-mau.firebaseapp.com",
+  databaseURL: "https://jomblo-mau-default-rtdb.firebaseio.com",
+  projectId: "jomblo-mau",
+  storageBucket: "jomblo-mau.firebasestorage.app",
+  messagingSenderId: "1034005784144",
+  appId: "1:1034005784144:web:370324a6608c97f3a99355",
+  measurementId: "G-074Y46SQJ5"
+};
+
+// Export config untuk dipakai di script.js
+window.firebaseConfig = firebaseConfig;
